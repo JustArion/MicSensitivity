ce51c504b69b5dae7099996b24bf593614699ed7
arion <53127594+Arion-Kun@users.noreply.github.com>
2020-11-24 21:44:53 +0200

    Main Commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+[assembly: MelonInfo(typeof(Dawn.Mic.MicSensitivity), Dawn.Update.AssemblyInfo.Name, Dawn.Update.AssemblyInfo.Version + Dawn.Update.AssemblyInfo.Version, Dawn.Update.AssemblyInfo.Authors)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonOptionalDependencies("UIExpansionKit")]
+namespace Dawn.Update
+{
+    internal static class AssemblyInfo
+    {
+        internal const string Name = "MicSensitivity";
+
+        internal const string Version = "1.0";
+
+        internal const string Description = "";
+
+        internal const string Authors = "@arion#1223";
+    }
+}
---
+++ MicSensitivity.cs
@@ -0,0 +1,81 @@
+﻿namespace Dawn.Mic
+{
+    using System;
+    using System.Collections;
+    using MelonLoader;
+    using UnityEngine;
+    using System.Linq;
+    internal class MicSensitivity : MelonMod
+    {
+        #region MelonMod Native
+        public override void OnApplicationStart()
+        {
+            MelonPrefs.RegisterBool("MicSensitivity", "Mic - Enable Mic Sensitivity Mod", false);
+            MelonPrefs.RegisterFloat("MicSensitivity", "Mic - Microphone Sensitivity", 100);
+            InternalConfigRefresh();
+        }
+        public override void OnLevelWasLoaded(int level)
+        {
+            switch (level) //Prevents being called 3x
+            {
+                case 0:
+                case 1:
+                    break;
+                default:
+                    MelonCoroutines.Start(USpeakerAdjust());
+                    break;
+            }
+        }
+        public override void OnModSettingsApplied()
+        {
+            InternalConfigRefresh();
+            if (!UseMod) return;
+            #if Unobfuscated
+            CurrentUser()._uSpeaker.VolumeThresholdRMS = MicSensitivityValue; //field_Private_USpeaker_0
+            #else
+            CurrentUser().field_Private_USpeaker_0.VolumeThresholdRMS = MicSensitivityValue;
+            #endif
+        }
+        #endregion
+        #region The Actual Mod
+        private static float MicSensitivityValue;
+        private static bool UseMod;
+        private static void InternalConfigRefresh() //The Divide by 10k sets it back to a managable float number
+        {
+            MicSensitivityValue = MelonPrefs.GetFloat("MicSensitivity", "Mic - Microphone Sensitivity") / 10000;
+            UseMod = MelonPrefs.GetBool("MicSensitivity", "Mic - Enable Mic Sensitivity Mod");
+        }
+        
+        private static CurrentUserDelegate CurrentUserInstance;
+        internal delegate VRCPlayer CurrentUserDelegate();
+        internal static CurrentUserDelegate CurrentUser //Cached CurrentUser
+        {
+            get
+            {
+                if (CurrentUserInstance != null) return CurrentUserInstance;
+                var MethodInfo = typeof(VRCPlayer).GetMethods().First(x => x.ReturnType == typeof(VRCPlayer));
+                CurrentUserInstance = (CurrentUserDelegate)Delegate.CreateDelegate(typeof(CurrentUserDelegate), MethodInfo);
+                return CurrentUserInstance;
+            }
+        }
+        
+        private static IEnumerator USpeakerAdjust()
+        {
+            for (;;)
+            {
+                if (CurrentUser() != null)
+                {
+                    if (!UseMod) yield break;
+                    #if Unobfuscated
+                    CurrentUser()._uSpeaker.VolumeThresholdRMS = MicSensitivityValue; //field_Private_USpeaker_0
+                    #else
+                    CurrentUser().field_Private_USpeaker_0.VolumeThresholdRMS = MicSensitivityValue;
+                    #endif
+                    yield break;
+                }
+                yield return new WaitForSeconds(1);
+            }
+        }
+        #endregion
+    }
+}
