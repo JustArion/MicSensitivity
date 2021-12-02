ec99bc6330544751e38bd6d88067ef2a4c6be9e1
arion <53127594+Arion-Kun@users.noreply.github.com>
2021-12-02 09:12:33 +0100

    Folder Refactor

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dawn.Update;
+using MelonLoader;
+using static Dawn.Update.AssemblyInfo;
+using ModType = Dawn.Mic.MicSensitivity;
+
+[assembly: MelonInfo(typeof(ModType), Name, AssemblyInfo.Version, Authors)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.DarkCyan)]
+[assembly: MelonOptionalDependencies("UIExpansionKit")]
+
+namespace Dawn.Update
+{
+    internal static class AssemblyInfo
+    {
+        internal const string Name = "MicSensitivity";
+
+        internal const string Version = "1.4.4";
+
+        internal const string Description = "";
+
+        internal const string Authors = "arion#1223";
+    }
+}
